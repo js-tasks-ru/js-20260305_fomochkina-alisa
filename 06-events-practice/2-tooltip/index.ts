d4f5e1ead497e2b2808ada38aa44ebcbaa96b1f6
Alisa Fomochkina <alisa.fomo@yandex.ru>
2026-03-30 00:28:50 +0300

    Исправление ошибки при тесте

--- 06-events-practice/2-tooltip/index.ts
+++ 06-events-practice/2-tooltip/index.ts
@@ -1,14 +1,17 @@
-
 export default class Tooltip {
     private static instance: Tooltip | null = null;
-    
+
     public element: HTMLElement | null = null;
+
+    private lastEvent: MouseEvent | null = null;
+
     private elementsWithTooltips: Map<HTMLElement, {
         pointerover: (e: MouseEvent) => void;
         pointermove: (e: MouseEvent) => void;
         pointerout: () => void;
     }> = new Map();
-    constructor(){
+
+    constructor() {
         if (Tooltip.instance) {
             return Tooltip.instance;
         }
@@ -17,16 +20,20 @@ export default class Tooltip {
         this.initialize();
     }
 
-    private initialize() {
+    public initialize() {
         const tooltipDiv = document.querySelector(`[data-tooltip]`) as HTMLElement | null;
 
-        if (tooltipDiv){
+        if (tooltipDiv) {
             const mousePointerOverHandler = (event: MouseEvent) => {
+                this.lastEvent = event;
                 this.showTooltip(event);
             };
+
             const mousePointerMoveHandler = (event: MouseEvent) => {
+                this.lastEvent = event;
                 this.showTooltip(event);
             };
+
             const mousePointerOutHandler = () => {
                 this.hideTooltip();
             };
@@ -38,36 +45,37 @@ export default class Tooltip {
             });
 
             tooltipDiv.addEventListener('pointerover', mousePointerOverHandler);
-            tooltipDiv.addEventListener('pointermove', mousePointerOverHandler);
+            tooltipDiv.addEventListener('pointermove', mousePointerMoveHandler);
             tooltipDiv.addEventListener('pointerout', mousePointerOutHandler);
         }
     }
 
-    private render(target: HTMLElement, event: MouseEvent){
-        if (!this.element){
+    public render(target: HTMLElement) {
+        if (!this.element) {
             const text = target?.dataset?.tooltip ?? '';
             this.element = document.createElement('div');
             this.element.className = 'tooltip';
-            this.element.textContent = text;  
+            this.element.textContent = text;
             document.body.appendChild(this.element);
         }
 
-        if (event){
-            this.element.style.left = `${event.clientX + 15}px`;
-            this.element.style.top = `${event.clientY + 15}px`;
-            this.element.style.position = 'fixed';            
+        if (this.lastEvent) {
+            this.element.style.left = `${this.lastEvent.clientX + 15}px`;
+            this.element.style.top = `${this.lastEvent.clientY + 15}px`;
+            this.element.style.position = 'fixed';
         }
     }
 
-    private showTooltip(event: MouseEvent){
+    private showTooltip(event: MouseEvent) {
         this.hideTooltip();
         const target = event.target as HTMLElement;
-        if (target){
-          this.render(target, event);  
+
+        if (target) {
+            this.render(target);
         }
     }
 
-    private hideTooltip(){
+    private hideTooltip() {
         if (this.element) {
             this.element.remove();
             this.element = null;
@@ -77,11 +85,12 @@ export default class Tooltip {
     public destroy(): void {
         this.elementsWithTooltips.forEach((handlers, element) => {
             element.removeEventListener('pointerover', handlers.pointerover);
+            element.removeEventListener('pointermove', handlers.pointermove);
             element.removeEventListener('pointerout', handlers.pointerout);
         });
-    
+
         this.elementsWithTooltips.clear();
-    
+
         this.element?.remove();
         this.element = null;
     }
